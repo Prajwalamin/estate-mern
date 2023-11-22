c8d552c49b2d1719fb17e5dd1332d44c095ae027
Prajwal Amin <prajwalmufc@gmail.com>
2023-11-22 12:44:36 +0000

    Adding Pages to the frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
 
 const App = () => {
-  return (
-    <div className='text-red-500 bg-green-300'>App</div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/signin' element={<SignIn/>}/>
+    <Route path='/signout' element={<SignOut/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/profile' element={<Profile/>}/>
+
+  </Routes>
+  </BrowserRouter>
 }
 
 export default App
