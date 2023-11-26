8b47b552b48ae478dc8f09249de450a988a21047
Prajwal Amin <prajwalmufc@gmail.com>
2023-11-26 14:07:58 +0000

    redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
 
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   // functions
   const handleChange = (e) => {
@@ -20,7 +26,7 @@ const SignIn = () => {
     e.preventDefault();
 
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -31,16 +37,13 @@ const SignIn = () => {
 
       const data = await res.json();
       if (data.success == false) {
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {}
-    setLoading(false);
-    setError(error.message);
+    dispatch(signInFailure(error.message));
   };
 
   return (
@@ -65,7 +68,7 @@ const SignIn = () => {
           disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-80 disabled:opacity-80"
         >
-          {loading ? "Loading.." : "Sign Up"}
+          {loading ? "Loading.." : "Sign In"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
